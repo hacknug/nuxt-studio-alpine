d3e2ff9d9590ff9bc2cc1ad75f509dfc2360586e
Nestor Vera <nestorvera@me.com>
2023-09-01 15:58:58 +0200

    Add placeholder content from `metabloggism`

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Metabloggism',
+    description: 'AI engineer and tech adventurer. I love learning so I learned Machine Learning for learning how to make Machines learn, but in my way I learned that I had learned for learning how to make Machines Learn to Learn. You may note my interest in Meta-Learning.',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -9,7 +9,7 @@ export default defineAppConfig({
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: '', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
@@ -18,21 +18,29 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: true,
+
+        // our github repository
+        repository: 'https://github.com/Metabloggism/metabloggism.github.io',
+
+        text: 'Metabloggism'
       },
       navigation: true, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
+      alignment: '', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: '',
+      instagram: '',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      github: 'https://github.com/MrLeylo'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
